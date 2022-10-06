36ad543022996eae80a81e6abb21dcae8e895400
Jessy Provenzal <jessyprovenzal@hotmail.com>
2022-10-06 11:00:09 +0200

    First Commit : Creationg of class.py

---
+++ class.py
@@ -0,0 +1,22 @@
+# Password Guesser class
+
+import random
+import string
+
+class PasswordGuesser:
+    def __init__(self, password):
+        self.password = password
+        self.guesses = 0
+
+    def guess(self):
+        self.guesses += 1
+        return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(len(self.password)))
+
+    def get_guesses(self):
+        return self.guesses
+
+    def get_password(self):
+        return self.password
+
+    def is_correct(self, guess):
+        return guess == self.password
